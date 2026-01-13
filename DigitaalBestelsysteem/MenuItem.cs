a97c11d92ce6b14ab784921b4d7b85c77f7fa1ce
bradleyscodes <b.malasch@hotmail.com>
2026-01-13 17:19:43 +0100

    Klasse MenuItem aangemaakt

---
+++ DigitaalBestelsysteem/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitaalBestelsysteem
+{
+    internal class MenuItem
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; } = "";
+        public decimal Prijs { get; set; }
+        public string Categorie { get; set; } = "Overig";
+        public string Omschrijving { get; set; } = "Geen omschrijving";
+        public bool IsDagfavoriet { get; set; } 
+
+    }
+}
