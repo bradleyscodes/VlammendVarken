42fc7e4f9bb267be21c9edf480109ae4b7ad7c70
bradleyscodes <b.malasch@hotmail.com>
2026-01-16 18:44:07 +0100

    Klasse OrderLine aangemaakt en bij Klasse MenuItem internal naar public gezet.

---
+++ DigitaalBestelsysteem/OrderLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitaalBestelsysteem
+{
+    public class OrderLine
+    {
+        public MenuItem Item { get; set; }
+        public int Quantity { get; set; }
+        public string Customization { get; set; }
+
+        // Bereken de totale prijs voor deze orderregel
+        public decimal LineTotal => Item.Price * Quantity;
+
+        // Constructor
+        public OrderLine(MenuItem item, int quantity, string customization = "")
+        {
+            Item = item;
+            Quantity = quantity;
+            Customization = customization;
+        }
+    }
+}
