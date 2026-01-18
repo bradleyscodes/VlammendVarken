b126d2d6f804fb53e558f1fd5834665ffa48b4c8
bradleyscodes <b.malasch@hotmail.com>
2026-01-18 13:04:14 +0100

    Klasse OrderStatus aanmaken

---
+++ DigitaalBestelsysteem/OrderStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitaalBestelsysteem
+{
+    public class OrderStatus
+    {
+        public enum Status
+        {
+            Geplaatst = 1,
+            InBehandeling = 2,
+            Klaar = 3,
+            Geserveerd = 4,
+        }
+    }
+}
