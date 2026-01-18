4c349efcfdd647395c57b4b84a5ad6dda654dbe7
bradleyscodes <b.malasch@hotmail.com>
2026-01-18 20:26:02 +0100

    Overerving toegepast.

--- DigitaalBestelsysteem/MenuItem.cs
+++ DigitaalBestelsysteem/MenuItem.cs
@@ -6,21 +6,16 @@ using System.Threading.Tasks;
 
 namespace DigitaalBestelsysteem
 {
-    public class MenuItem
+    public class MenuItem : Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
         public string Category { get; set; } 
         public string Description { get; set; } 
         public bool IsFavoriteDishOfTheDay { get; set; } = false;
 
         //Constructor
-        public MenuItem(int id, string name, decimal price, string category, string description, bool isFavoriteDishOfTheDay = false)
+        public MenuItem(int id, string name, decimal price, string category, string description, bool isFavoriteDishOfTheDay = false) 
+            : base(id, name, price)
         {
-            Id = id;
-            Name = name;
-            Price = price;
             Category = category;
             Description = description;
             IsFavoriteDishOfTheDay = isFavoriteDishOfTheDay;
---
+++ DigitaalBestelsysteem/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitaalBestelsysteem
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        //Constructor
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+            
+    }
+}
